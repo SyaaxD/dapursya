1b08be306f1092ecedce39bd7d1d28175d449b19
SyaaxD <syahdanattala04@gmail.com>
2026-07-18 15:51:52 +0700

    Final debug

--- api/submit.js
+++ api/submit.js
@@ -1,7 +1,9 @@
 export default function handler(req, res) {
   res.json({
-    env: Object.keys(process.env).filter(key =>
-      key.includes("GOOGLE") || key.includes("SPREAD") || key.includes("VERCEL")
-    )
+    google: !!process.env.GOOGLE_SERVICE_ACCOUNT,
+    spreadsheet: !!process.env.SPREADSHEET_ID,
+    project: process.env.VERCEL_PROJECT_NAME,
+    env: process.env.VERCEL_ENV,
+    commit: process.env.VERCEL_GIT_COMMIT_SHA
   });
 }
